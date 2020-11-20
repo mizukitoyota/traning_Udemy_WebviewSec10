0aa191d68d3cda9b9c014e9ff247c64bbf96bb5c
MizukiToyota <mtoyota@system-renovate.co.jp>
2020-11-20 13:49:50 +0900

    骨組み作成

---
+++ WebviewSec10/WebviewSec10/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  WebviewSec10
+//
+//  Created by Training on 2020/11/20.
+//  Copyright © 2020 training. All rights reserved.
+//
+
+import UIKit
+//パーツの配置の仕方
+//
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+}
+
